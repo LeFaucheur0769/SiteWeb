3c402384b963073ee85806e0a80e61f865689ac9
LeFaucheur0769 <tristanburdin1@gmail.com>
2024-02-29 11:52:44 +0100

    modified:   marmoutier-jsx/src/components/visual/timetable_input.jsx

--- marmoutier-jsx/src/components/visual/timetable_input.jsx
+++ marmoutier-jsx/src/components/visual/timetable_input.jsx
@@ -1,34 +1,69 @@
 import React, { useState } from 'react';
 
 const TimetableInput = () => {
-    const [value, setValue] = useState('');
+    const [value_start, setValue_start] = useState('');
+    const [value_end, setValue_end] = useState('');
     const [error, setError] = useState('');
 
-    const handleChange = (event) => {
-        const inputValue = event.target.value.trim();
-        setValue(inputValue);
+    const handleChange_start = (event) => {
+        const inputValue_start = event.target.value.trim();
+        setValue_start(inputValue_start);
 
-        if (validateTimetable(inputValue)) {
+        if (validateTimetable(inputValue_start)) {
             setError('');
+            console.log(inputValue_start)
         } else {
             setError('Please enter a valid timetable in the format "HHhMMm-HHhMMm"');
         }
+        if (validateTimetable(inputValue_start) && inputValue_start < value_end && value_end != null) {
+            setError('');
+            console.log(inputValue_start);
+        } else {
+            setError('Please enter a valid timetable in the format "HHhMMm-HHhMMm" and ensure it is lower than the end time');
+        }
+
+    };
+
+    const handleChange_end = (event) => {
+        const inputValue_end = event.target.value.trim();
+        setValue_end(inputValue_end);
+
+        if (validateTimetable(inputValue_end)) {
+            setError('');
+            console.log(inputValue_end)
+        } else {
+            setError('Please enter a valid timetable in the format "HHhMMm-HHhMMm"');
+        }
+
     };
 
     const validateTimetable = (input) => {
         var regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+        //print in console the result and then return it 
         return regex.test(input);
     };
 
     return (
         <div>
-            <label htmlFor="timetable" style={{marginRight : "20px", marginLeft : "20px"}}>Horaires  :</label>
+            <label htmlFor="timetable" style={{marginRight : "20px", marginLeft : "20px"}}>Horaires  </label>
             <input 
                 type="time" 
-                id="timetable"
+                id="timetable_start"
                 name="timetable" 
-                value={value}
-                onChange={handleChange}
+                value={value_start}
+                onChange={handleChange_start}
+                pattern="(0[1-9]|1[0-2])h([0-5][0-9])m-(0[1-9]|1[0-2])h([0-5][0-9])m" 
+                style={{ padding: '6px 10px', fontSize: '16px', borderRadius: '4px', border: '1px solid #ccc' }}
+
+                required 
+            />
+            <label htmlFor="timetable" style={{marginRight : "20px", marginLeft : "30px"}}>:  </label>
+            <input 
+                type="time" 
+                id="timetable_end"
+                name="timetable" 
+                value={value_end}
+                onChange={handleChange_end}
                 pattern="(0[1-9]|1[0-2])h([0-5][0-9])m-(0[1-9]|1[0-2])h([0-5][0-9])m" 
                 style={{ padding: '6px 10px', fontSize: '16px', borderRadius: '4px', border: '1px solid #ccc' }}
 
@@ -46,5 +81,6 @@ const TimetableInput = () => {
         </div>
     );
 };
+
 
 export default TimetableInput;
