274eca0b1b99aeec4bd6cf0be0f5c80a6eee8d49
LeFaucheur0769 <kali.lefaucheur@gmail.com>
2024-03-05 17:44:28 +0100

    modified:   marmoutier-jsx/src/components/visual/Calendar.jsx 	modified:   marmoutier_backend_expressjs/src/events.json

--- marmoutier-jsx/src/components/visual/Calendar.jsx
+++ marmoutier-jsx/src/components/visual/Calendar.jsx
@@ -6,6 +6,18 @@ import 'react-calendar/dist/Calendar.css';
 import MyTimetable from '../timetable';
 
 function MyCalendar() {
+
+/* test to see how i can transform string into date */
+  const dateString = "Mar 14 2024";
+  const parsedDate = new Date(dateString);
+  const year = parsedDate.getFullYear();
+  const month = (parsedDate.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+  const day = parsedDate.getDate().toString().padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}`;
+
+  console.log(formattedDate);
+
+/* some variables for the calendar */
   const [date, setDate] = useState(new Date());
   const [selectedDates, setSelectedDates] = useState(new Set());
   const [selectedDateIsGreen, setSelectedDateIsGreen] = useState(false);
@@ -28,6 +40,7 @@ function MyCalendar() {
   const handleDateChange = (newDate) => {
     setDate(newDate);
     setSelectedDateIsGreen(selectedDates.has(newDate.toDateString()));
+    console.log('Selected date is ' + newDate.toDateString());
   };
 
   // This code defines a function called toggleView that toggles the value of showCalendar. If showCalendar is true, it will be set to false, and vice versa.
@@ -60,7 +73,7 @@ function MyCalendar() {
         </div>
       ) : (
         <div>
-          <MyTimetable />
+          <MyTimetable parsedData={"Thu Nov 23 2023"}/>
           <button onClick={toggleView}>Retour au Calendrier</button>
         </div>
       )}
