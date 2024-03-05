84fbf588a64a2621caf43f12d32b4dbda2801b96
LeFaucheur0769 <kali.lefaucheur@gmail.com>
2024-03-05 18:05:03 +0100

    Afficher uniquement horaire choisi marche

--- marmoutier-jsx/src/components/visual/Calendar.jsx
+++ marmoutier-jsx/src/components/visual/Calendar.jsx
@@ -7,15 +7,6 @@ import MyTimetable from '../timetable';
 
 function MyCalendar() {
 
-/* test to see how i can transform string into date */
-  const dateString = "Mar 14 2024";
-  const parsedDate = new Date(dateString);
-  const year = parsedDate.getFullYear();
-  const month = (parsedDate.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-  const day = parsedDate.getDate().toString().padStart(2, '0');
-  const formattedDate = `${year}-${month}-${day}`;
-
-  console.log(formattedDate);
 
 /* some variables for the calendar */
   const [date, setDate] = useState(new Date());
@@ -43,12 +34,34 @@ function MyCalendar() {
     console.log('Selected date is ' + newDate.toDateString());
   };
 
+  /* test to see how i can transform string into date */
+  const dateString = date.toDateString();
+  const parsedDate = new Date(dateString);
+  const year = parsedDate.getFullYear();
+  const month = (parsedDate.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+  const day = parsedDate.getDate().toString().padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}`;
+  console.log(formattedDate);
+  
+
   // This code defines a function called toggleView that toggles the value of showCalendar. If showCalendar is true, it will be set to false, and vice versa.
   // It is used to toggle between showing the calendar and showing the timetable if the selectedDateIsGreen is true.
   const toggleView = () => {
     setShowCalendar(!showCalendar);
   };
 
+
+  /**
+   * A function that generates content for a tile based on the provided date.
+   *
+   * @param {object} date - The date for which the content is generated
+   * @return {JSX.Element | null} The content for the tile, or null if there is no content
+   */
+
+/* 
+  This is a JavaScript function called tileContent that takes a date object as an argument. 
+  It checks if the provided date is in a set called selectedDates and returns a div with a class "selected-date" if it is, otherwise it returns null. 
+*/
   const tileContent = ({ date }) => {
     if (selectedDates.has(date.toDateString())) {
       return <div className="selected-date"></div>;
@@ -73,7 +86,7 @@ function MyCalendar() {
         </div>
       ) : (
         <div>
-          <MyTimetable parsedData={"Thu Nov 23 2023"}/>
+          <MyTimetable parsedData={formattedDate}/>
           <button onClick={toggleView}>Retour au Calendrier</button>
         </div>
       )}
